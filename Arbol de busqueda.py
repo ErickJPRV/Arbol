392fe7327c0331404c6267436e74d60c6e33fcf1
ErickJPRV <132368878+ErickJPRV@users.noreply.github.com>
2023-05-31 17:47:20 -0500

    Add files via upload

--- Arbol de busqueda.py
+++ Arbol de busqueda.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf as pdf_backend
 iteraciones=[]
 ingreso_de_datos=[]
+global encontrado
+global p
 class Nodo:
     def __init__(self, valor):
         self.valor = valor
         self.der = None
         self.izq = None
 def Busqueda(item,a):
+    global encontrado
     while a is not None:
         if item > a.valor:
             iteraciones.append(a.valor)
@@ -19,10 +22,11 @@ def Busqueda(item,a):
             encontrado = True
             break
     if encontrado:
-        print("Item encontrado")
+        pass
     else:
-        print("Objeto no encontrado")    
+        encontrado=False    
 def Insertar(item, a):
+    global p
     while True:
         ingreso_de_datos.append(a.valor)
         if item > a.valor:
@@ -37,14 +41,24 @@ def Insertar(item, a):
                 break
             else:
                 a = a.izq
+        elif item==a.valor:
+            p=True
+            break
     pass
 def graficar_arbol(nodo, x, y, dx, dy,item,item2):
+    global encontrado
     if nodo is None:
         return
     plt.text(x, y, str(nodo.valor), ha='center', va='center', 
              bbox=dict(facecolor='white', edgecolor='black', boxstyle='circle'))
-    plt.text(0, 10, 'Las iteraciones para encontrar el 80 fueron ' + "".join(iteraciones)+str(item), ha='center', va='center', bbox=dict(facecolor='white', edgecolor='black', boxstyle='square'))
-    plt.text(0, 15, 'Las iteraciones para ingresar el numero fueron ' + "".join(ingreso_de_datos)+str(item), ha='center', va='center', bbox=dict(facecolor='white', edgecolor='black', boxstyle='square'))
+    if(encontrado==False):
+        plt.text(0, 10, 'No se pudo encontrar el numero seleccionado', ha='center', va='center', bbox=dict(facecolor='white', edgecolor='black', boxstyle='square'))
+    elif(encontrado==True):
+        plt.text(0, 10, 'Las iteraciones para encontrar el n fueron ' + "".join(iteraciones)+str(item), ha='center', va='center', bbox=dict(facecolor='white', edgecolor='black', boxstyle='square'))
+    if(p==True):
+        plt.text(0, 15, 'El numero que quiere insertar ya existe', ha='center', va='center', bbox=dict(facecolor='white', edgecolor='black', boxstyle='square'))    
+    elif(p==False):
+        plt.text(0, 15, 'Las iteraciones para ingresar el numero fueron ' + "".join(ingreso_de_datos)+str(item2), ha='center', va='center', bbox=dict(facecolor='white', edgecolor='black', boxstyle='square'))
     x_izq = x - dx
     x_der = x + dx
     y_nivel = y - dy
@@ -77,24 +91,19 @@ J40.der=L50
 J40.izq=K40
 F90.der=I95
 F90.izq=H80
-item = 80
+item = 50  ######Numero que desea Buscar
+item2= 70 ####Numero que desea ingresar
 a = A60
 encontrado = False
 Busqueda(item,a)
-Insertar(21,a)
+Insertar(item2,a)
 for i in range(0,len(iteraciones)):
     iteraciones[i]=str(iteraciones[i])+'->'
 for i in range(0,len(ingreso_de_datos)):
     ingreso_de_datos[i]=str(ingreso_de_datos[i])+'->'
 pdf = pdf_backend.PdfPages("arbol.pdf")
 fig = plt.figure(figsize=(8, 6))
-graficar_arbol(a, 0, 0, 50, 20,item,21)
+graficar_arbol(a, 0, 0, 50, 20,item,item2)
 pdf.savefig(fig)
 plt.close(fig)
 pdf.close()
-
-
-
-
-
-
